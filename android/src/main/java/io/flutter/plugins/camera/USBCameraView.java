6cb1c0750bdb48c49afa535732c1054539d53cf3
Bach Ngoc Hoai <bach.ngoc.hoai@sun-asterisk.com>
2021-05-22 08:18:14 +0700

    Add USBCamera view

---
+++ android/src/main/java/io/flutter/plugins/camera/USBCameraView.java
@@ -0,0 +1,521 @@
+package io.flutter.plugins.camera;
+import android.graphics.Bitmap;
+import android.graphics.SurfaceTexture;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+import android.view.Surface;
+
+import com.serenegiant.glutils.EGLBase;
+import com.serenegiant.glutils.GLDrawer2D;
+import com.serenegiant.glutils.es1.GLHelper;
+import com.serenegiant.usb.encoder.IVideoEncoder;
+import com.serenegiant.usb.encoder.MediaEncoder;
+import com.serenegiant.usb.encoder.MediaVideoEncoder;
+import com.serenegiant.usb.widget.CameraViewInterface;
+import com.serenegiant.utils.FpsCounter;
+
+public class USBCameraView implements CameraViewInterface {
+
+    private static final boolean DEBUG = true;
+    private static final String TAG = "UVCCameraTextureView";
+
+    private boolean mHasSurface;
+    private RenderHandler mRenderHandler;
+    private final Object mCaptureSync = new Object();
+    private float ratio = 1.0f;
+
+    /**
+     * for calculation of frame rate
+     */
+    private final FpsCounter mFpsCounter = new FpsCounter();
+
+    public USBCameraView() {}
+
+    public void initRender(SurfaceTexture texture, int width, int height) {
+        ratio = (float) width / (float) height;
+        mRenderHandler = RenderHandler.createHandler(mFpsCounter, texture, width, height);
+        mHasSurface = true;
+    }
+
+    @Override
+    public void onResume() {
+    }
+
+    @Override
+    public void onPause() {
+        if (mRenderHandler != null) {
+            mRenderHandler.release();
+            mRenderHandler = null;
+        }
+    }
+
+    public void destroy() {
+        if (mRenderHandler != null) {
+            mRenderHandler.release();
+            mRenderHandler = null;
+        }
+        mHasSurface = false;
+        if (mPreviewSurface != null) {
+            mPreviewSurface.release();
+            mPreviewSurface = null;
+        }
+    }
+
+    @Override
+    public boolean hasSurface() {
+        return mHasSurface;
+    }
+
+    /**
+     * capture preview image as a bitmap
+     * this method blocks current thread until bitmap is ready
+     * if you call this method at almost same time from different thread,
+     * the returned bitmap will be changed while you are processing the bitmap
+     * (because we return same instance of bitmap on each call for memory saving)
+     * if you need to call this method from multiple thread,
+     * you should change this method(copy and return)
+     */
+    @Override
+    public Bitmap captureStillImage(int width, int height) {
+        return null;
+    }
+
+    @Override
+    public SurfaceTexture getSurfaceTexture() {
+        return mRenderHandler != null ? mRenderHandler.getPreviewTexture() : null;
+    }
+
+    private Surface mPreviewSurface;
+
+    @Override
+    public Surface getSurface() {
+        if (mPreviewSurface == null) {
+            final SurfaceTexture st = getSurfaceTexture();
+            if (st != null) {
+                mPreviewSurface = new Surface(st);
+            }
+        }
+        return mPreviewSurface;
+    }
+
+    @Override
+    public void setVideoEncoder(final IVideoEncoder encoder) {
+        if (mRenderHandler != null)
+            mRenderHandler.setVideoEncoder(encoder);
+    }
+
+    @Override
+    public void setCallback(final Callback callback) {
+    }
+
+    public void resetFps() {
+        mFpsCounter.reset();
+    }
+
+    /**
+     * update frame rate of image processing
+     */
+    public void updateFps() {
+        mFpsCounter.update();
+    }
+
+    /**
+     * get current frame rate of image processing
+     *
+     * @return
+     */
+    public float getFps() {
+        return mFpsCounter.getFps();
+    }
+
+    /**
+     * get total frame rate from start
+     *
+     * @return
+     */
+    public float getTotalFps() {
+        return mFpsCounter.getTotalFps();
+    }
+
+    @Override
+    public void setAspectRatio(double v) {
+
+    }
+
+    @Override
+    public void setAspectRatio(int width, int height) {
+
+    }
+
+    @Override
+    public double getAspectRatio() {
+        return ratio;
+    }
+
+    /**
+     * render camera frames on this view on a private thread
+     *
+     * @author saki
+     */
+    private static final class RenderHandler extends Handler
+            implements SurfaceTexture.OnFrameAvailableListener {
+
+        private static final int MSG_REQUEST_RENDER = 1;
+        private static final int MSG_SET_ENCODER = 2;
+        private static final int MSG_CREATE_SURFACE = 3;
+        private static final int MSG_RESIZE = 4;
+        private static final int MSG_TERMINATE = 9;
+
+        private RenderThread mThread;
+        private boolean mIsActive = true;
+        private final FpsCounter mFpsCounter;
+
+        public static final RenderHandler createHandler(final FpsCounter counter,
+                                                        final SurfaceTexture surface, final int width, final int height) {
+
+            final RenderThread thread = new RenderThread(counter, surface, width, height);
+            thread.start();
+            return thread.getHandler();
+        }
+
+        private RenderHandler(final FpsCounter counter, final RenderThread thread) {
+            mThread = thread;
+            mFpsCounter = counter;
+        }
+
+        public final void setVideoEncoder(final IVideoEncoder encoder) {
+            if (DEBUG) Log.v(TAG, "setVideoEncoder:");
+            if (mIsActive)
+                sendMessage(obtainMessage(MSG_SET_ENCODER, encoder));
+        }
+
+        public final SurfaceTexture getPreviewTexture() {
+            if (DEBUG) Log.v(TAG, "getPreviewTexture:");
+            if (mIsActive) {
+                synchronized (mThread.mSync) {
+                    sendEmptyMessage(MSG_CREATE_SURFACE);
+                    try {
+                        mThread.mSync.wait();
+                    } catch (final InterruptedException e) {
+                    }
+                    return mThread.mPreviewSurface;
+                }
+            } else {
+                return null;
+            }
+        }
+
+        public void resize(final int width, final int height) {
+            if (DEBUG) Log.v(TAG, "resize:");
+            if (mIsActive) {
+                synchronized (mThread.mSync) {
+                    sendMessage(obtainMessage(MSG_RESIZE, width, height));
+                    try {
+                        mThread.mSync.wait();
+                    } catch (final InterruptedException e) {
+                    }
+                }
+            }
+        }
+
+        public final void release() {
+            if (DEBUG) Log.v(TAG, "release:");
+            if (mIsActive) {
+                mIsActive = false;
+                removeMessages(MSG_REQUEST_RENDER);
+                removeMessages(MSG_SET_ENCODER);
+                sendEmptyMessage(MSG_TERMINATE);
+            }
+        }
+
+        @Override
+        public final void onFrameAvailable(final SurfaceTexture surfaceTexture) {
+            if (mIsActive) {
+                mFpsCounter.count();
+                sendEmptyMessage(MSG_REQUEST_RENDER);
+            }
+        }
+
+        @Override
+        public final void handleMessage(final Message msg) {
+            if (mThread == null) return;
+            switch (msg.what) {
+                case MSG_REQUEST_RENDER:
+                    mThread.onDrawFrame();
+                    break;
+                case MSG_SET_ENCODER:
+                    mThread.setEncoder((MediaEncoder) msg.obj);
+                    break;
+                case MSG_CREATE_SURFACE:
+                    mThread.updatePreviewSurface();
+                    break;
+                case MSG_RESIZE:
+                    mThread.resize(msg.arg1, msg.arg2);
+                    break;
+                case MSG_TERMINATE:
+                    Looper.myLooper().quit();
+                    mThread = null;
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+
+        private static final class RenderThread extends Thread {
+            private final Object mSync = new Object();
+            private final SurfaceTexture mSurface;
+            private RenderHandler mHandler;
+            private EGLBase mEgl;
+            /**
+             * IEglSurface instance related to this TextureView
+             */
+            private EGLBase.IEglSurface mEglSurface;
+            private GLDrawer2D mDrawer;
+            private int mTexId = -1;
+            /**
+             * SurfaceTexture instance to receive video images
+             */
+            private SurfaceTexture mPreviewSurface;
+            private final float[] mStMatrix = new float[16];
+            private MediaEncoder mEncoder;
+            private int mViewWidth, mViewHeight;
+            private final FpsCounter mFpsCounter;
+
+            /**
+             * constructor
+             *
+             * @param surface: drawing surface came from TexureView
+             */
+            public RenderThread(final FpsCounter fpsCounter, final SurfaceTexture surface, final int width, final int height) {
+                mFpsCounter = fpsCounter;
+                mSurface = surface;
+                mViewWidth = width;
+                mViewHeight = height;
+                setName("RenderThread");
+            }
+
+            public final RenderHandler getHandler() {
+                if (DEBUG) Log.v(TAG, "RenderThread#getHandler:");
+                synchronized (mSync) {
+                    // create rendering thread
+                    if (mHandler == null)
+                        try {
+                            mSync.wait();
+                        } catch (final InterruptedException e) {
+                        }
+                }
+                return mHandler;
+            }
+
+            public void resize(final int width, final int height) {
+                if (((width > 0) && (width != mViewWidth)) || ((height > 0) && (height != mViewHeight))) {
+                    mViewWidth = width;
+                    mViewHeight = height;
+                    updatePreviewSurface();
+                } else {
+                    synchronized (mSync) {
+                        mSync.notifyAll();
+                    }
+                }
+            }
+
+            public final void updatePreviewSurface() {
+                if (DEBUG) Log.i(TAG, "RenderThread#updatePreviewSurface:");
+                synchronized (mSync) {
+                    if (mPreviewSurface != null) {
+                        if (DEBUG) Log.d(TAG, "updatePreviewSurface:release mPreviewSurface");
+                        mPreviewSurface.setOnFrameAvailableListener(null);
+                        mPreviewSurface.release();
+                        mPreviewSurface = null;
+                    }
+                    mEglSurface.makeCurrent();
+                    if (mTexId >= 0) {
+                        mDrawer.deleteTex(mTexId);
+                    }
+                    // create texture and SurfaceTexture for input from camera
+                    mTexId = mDrawer.initTex();
+                    if (DEBUG) Log.v(TAG, "updatePreviewSurface:tex_id=" + mTexId);
+                    mPreviewSurface = new SurfaceTexture(mTexId);
+                    mPreviewSurface.setDefaultBufferSize(mViewWidth, mViewHeight);
+                    mPreviewSurface.setOnFrameAvailableListener(mHandler);
+                    // notify to caller thread that previewSurface is ready
+                    mSync.notifyAll();
+                }
+            }
+
+            public final void setEncoder(final MediaEncoder encoder) {
+                if (DEBUG) Log.v(TAG, "RenderThread#setEncoder:encoder=" + encoder);
+                if (encoder != null && (encoder instanceof MediaVideoEncoder)) {
+                    ((MediaVideoEncoder) encoder).setEglContext(mEglSurface.getContext(), mTexId);
+                }
+                mEncoder = encoder;
+            }
+
+            /*
+             * Now you can get frame data as ByteBuffer(as YUV/RGB565/RGBX/NV21 pixel format) using IFrameCallback interface
+             * with UVCCamera#setFrameCallback instead of using following code samples.
+             */
+/*			// for part1
+ 			private static final int BUF_NUM = 1;
+			private static final int BUF_STRIDE = 640 * 480;
+			private static final int BUF_SIZE = BUF_STRIDE * BUF_NUM;
+			int cnt = 0;
+			int offset = 0;
+			final int pixels[] = new int[BUF_SIZE];
+			final IntBuffer buffer = IntBuffer.wrap(pixels); */
+/*			// for part2
+			private ByteBuffer buf = ByteBuffer.allocateDirect(640 * 480 * 4);
+ */
+
+            /**
+             * draw a frame (and request to draw for video capturing if it is necessary)
+             */
+            public final void onDrawFrame() {
+                mEglSurface.makeCurrent();
+                // update texture(came from camera)
+                mPreviewSurface.updateTexImage();
+                // get texture matrix
+                mPreviewSurface.getTransformMatrix(mStMatrix);
+                // notify video encoder if it exist
+                if (mEncoder != null) {
+                    // notify to capturing thread that the camera frame is available.
+                    if (mEncoder instanceof MediaVideoEncoder)
+                        ((MediaVideoEncoder) mEncoder).frameAvailableSoon(mStMatrix);
+                    else
+                        mEncoder.frameAvailableSoon();
+                }
+                // draw to preview screen
+                mDrawer.draw(mTexId, mStMatrix, 0);
+                mEglSurface.swap();
+/*				// sample code to read pixels into Buffer and save as a Bitmap (part1)
+				buffer.position(offset);
+				GLES20.glReadPixels(0, 0, 640, 480, GLES20.GL_RGBA, GLES20.GL_UNSIGNED_BYTE, buffer);
+				if (++cnt == 100) { // save as a Bitmap, only once on this sample code
+					// if you save every frame as a Bitmap, app will crash by Out of Memory exception...
+					Log.i(TAG, "Capture image using glReadPixels:offset=" + offset);
+					final Bitmap bitmap = createBitmap(pixels,offset,  640, 480);
+					final File outputFile = MediaMuxerWrapper.getCaptureFile(Environment.DIRECTORY_DCIM, ".png");
+					try {
+						final BufferedOutputStream os = new BufferedOutputStream(new FileOutputStream(outputFile));
+						try {
+							try {
+								bitmap.compress(CompressFormat.PNG, 100, os);
+								os.flush();
+								bitmap.recycle();
+							} catch (IOException e) {
+							}
+						} finally {
+							os.close();
+						}
+					} catch (FileNotFoundException e) {
+					} catch (IOException e) {
+					}
+				}
+				offset = (offset + BUF_STRIDE) % BUF_SIZE;
+*/
+/*				// sample code to read pixels into Buffer and save as a Bitmap (part2)
+		        buf.order(ByteOrder.LITTLE_ENDIAN);	// it is enough to call this only once.
+		        GLES20.glReadPixels(0, 0, 640, 480, GLES20.GL_RGBA, GLES20.GL_UNSIGNED_BYTE, buf);
+		        buf.rewind();
+				if (++cnt == 100) {	// save as a Bitmap, only once on this sample code
+					// if you save every frame as a Bitmap, app will crash by Out of Memory exception...
+					final File outputFile = MediaMuxerWrapper.getCaptureFile(Environment.DIRECTORY_DCIM, ".png");
+			        BufferedOutputStream os = null;
+					try {
+				        try {
+				            os = new BufferedOutputStream(new FileOutputStream(outputFile));
+				            Bitmap bmp = Bitmap.createBitmap(640, 480, Bitmap.Config.ARGB_8888);
+				            bmp.copyPixelsFromBuffer(buf);
+				            bmp.compress(Bitmap.CompressFormat.PNG, 90, os);
+				            bmp.recycle();
+				        } finally {
+				            if (os != null) os.close();
+				        }
+					} catch (FileNotFoundException e) {
+					} catch (IOException e) {
+					}
+				}
+*/
+            }
+
+/*			// sample code to read pixels into IntBuffer and save as a Bitmap (part1)
+			private static Bitmap createBitmap(final int[] pixels, final int offset, final int width, final int height) {
+				final Paint paint = new Paint(Paint.FILTER_BITMAP_FLAG);
+				paint.setColorFilter(new ColorMatrixColorFilter(new ColorMatrix(new float[] {
+						0, 0, 1, 0, 0,
+						0, 1, 0, 0, 0,
+						1, 0, 0, 0, 0,
+						0, 0, 0, 1, 0
+					})));
+
+				final Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+				final Canvas canvas = new Canvas(bitmap);
+
+				final Matrix matrix = new Matrix();
+				matrix.postScale(1.0f, -1.0f);
+				matrix.postTranslate(0, height);
+				canvas.concat(matrix);
+
+				canvas.drawBitmap(pixels, offset, width, 0, 0, width, height, false, paint);
+
+				return bitmap;
+			} */
+
+            @Override
+            public final void run() {
+                Log.d(TAG, getName() + " started");
+                init();
+                Looper.prepare();
+                synchronized (mSync) {
+                    mHandler = new RenderHandler(mFpsCounter, this);
+                    mSync.notify();
+                }
+
+                Looper.loop();
+
+                Log.d(TAG, getName() + " finishing");
+                release();
+                synchronized (mSync) {
+                    mHandler = null;
+                    mSync.notify();
+                }
+            }
+
+            private final void init() {
+                if (DEBUG) Log.v(TAG, "RenderThread#init:");
+                // create EGLContext for this thread
+                mEgl = EGLBase.createFrom(null, false, false);
+                mEglSurface = mEgl.createFromSurface(mSurface);
+                mEglSurface.makeCurrent();
+                // create drawing object
+                mDrawer = new GLDrawer2D(true);
+            }
+
+            private final void release() {
+                if (DEBUG) Log.v(TAG, "RenderThread#release:");
+                if (mDrawer != null) {
+                    mDrawer.release();
+                    mDrawer = null;
+                }
+                if (mPreviewSurface != null) {
+                    mPreviewSurface.release();
+                    mPreviewSurface = null;
+                }
+                if (mTexId >= 0) {
+                    GLHelper.deleteTex(mTexId);
+                    mTexId = -1;
+                }
+                if (mEglSurface != null) {
+                    mEglSurface.release();
+                    mEglSurface = null;
+                }
+                if (mEgl != null) {
+                    mEgl.release();
+                    mEgl = null;
+                }
+            }
+        }
+    }
+}
